de959e0c156fe52a6fe9210b4258e55b37e8c8fd
saurabhsingh <saurabhsingh6844@gmail.com>
2025-04-27 18:01:36 +0530

    Array Question

---
+++ src/ArraySumAndAverage.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class ArraySumAndAverage {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the number of  element ");
+        int size = input.nextInt();
+
+        int [] numArray =new int[size];
+        int i = 0;
+        while (i<size){
+            System.out.println("Please enter element no "+(i+1)+": ");
+            numArray[i]=input.nextInt();
+            i++;
+        }
+        long sum = sum(numArray);
+       double avg = average(numArray);
+        System.out.println("Sum of the numbers is : "+sum);
+        System.out.println("Average of the numbers is : "+avg);
+
+    }
+    public static long sum(int[] numArray){
+        long  sum =0;
+        int i=0;
+        while (i<numArray.length){
+            sum +=numArray[i];
+            i++;
+        }
+        return sum;
+    }
+    public static double average(int[]numArray){
+        double sum=sum(numArray);
+        return (sum/numArray.length);
+    }
+}
---
+++ src/OccurrencesArray.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class OccurrencesArray {
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+//for create an array
+        System.out.println("Enter the size of array : ");
+        int size = input.nextInt();
+        int [] numArray = new int[size];
+        int i=0;
+        while (i<size){
+            System.out.println("Enter the element value "+(i+1)+" : ");
+            numArray[i]=input.nextInt();
+            i++;
+        }
+//Is used for which number you want to find int the array
+        System.out.println("Enter the number you want to found in the array");
+        int num=input.nextInt();
+
+        int occurrences=occurrences(numArray,num);
+        System.out.println("Your element was found "+occurrences+" times in the array ");
+    }
+    public static int occurrences(int[] numArray, int num){
+        int occ=0;
+        int i=0;
+        while (i<numArray.length){
+            if (numArray[i]==num){
+                occ++;
+            }
+            i++;
+        }
+        return occ;
+    }
+}
